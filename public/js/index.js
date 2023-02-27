5a63a58189016bb89013dbb39655d580cd66c0e8
dangtus <dangtu9520@gmail.com>
2023-02-27 13:16:02 +0700

    them 3 man hinh bao gom bat dau, tra loi lai, ket thuc

--- public/js/index.js
+++ public/js/index.js
@@ -1,108 +1,239 @@
-// Select elements
-const answers = document.querySelectorAll(".answers .card");
-const buttons = document.querySelectorAll("button");
-const submitButton = document.getElementById("submit-button");
-const resultOverlay = document.getElementById("result-overlay");
-const resultModal = document.getElementById("result-modal");
-const resultText = document.getElementById("result-text");
-const resultMessage = document.getElementById("result-message");
-const resultIcon = document.getElementById("result-icon");
-const nextButton = document.getElementById("next-button");
-var answer_text = null;
+// Chọn các phần tử trong màn hình
+const startScreenElement = document.getElementById("start-screen"); // Phần tử màn hình bắt đầu
+const quizScreen = document.getElementById("quiz-screen"); // Phần tử màn hình trả lời câu hỏi
+const retryScreen = document.getElementById("retry-screen"); // Phần tử màn hình thử lại
+const resultScreen = document.getElementById("result-screen"); // Phần tử màn hình kết quả
 
-// Load questions and answers on page load
-window.onload = function () {
-    get_list_questionAnswer();
-    load_audio();
-};
+// Chọn các phần tử trong màn hình trả lời câu hỏi
+const questionElement = document.getElementById("question"); // Phần tử câu hỏi
+const answerElements = document.querySelectorAll(".answers .card"); // Mảng các phần tử trả lời
+const buttonElements = document.querySelectorAll("button"); // Mảng các nút bấm
+const submitButton = document.getElementById("submit-button"); // Nút bấm để nộp bài
+const resultOverlayElement = document.getElementById("result-overlay"); // Phần tử lớp phủ kết quả
+const resultModalElement = document.getElementById("result-modal"); // Phần tử kết quả
+const resultTextElement = document.getElementById("result-text"); // Phần tử văn bản kết quả
+const resultMessageElement = document.getElementById("result-message"); // Phần tử thông báo kết quả
+const resultIconElement = document.getElementById("result-icon"); // Phần tử biểu tượng kết quả
+const nextButton = document.getElementById("next-button"); // Nút bấm để chuyển sang câu hỏi tiếp theo
+const audioCorrect = document.getElementById("audio-correct"); // Phần tử âm thanh khi trả lời đúng
+const audioWrong = document.getElementById("audio-wrong"); // Phần tử âm thanh khi trả lời sai
+const loadElement = document.querySelector(".load-wrapper"); // Phần tử hiển thị khi đang tải
+var selectedAnswer = null; // Biến chứa phần tử trả lời đã chọn
 
-// Add event listener for selecting an answer
-answers.forEach((answer) => {
-    answer.addEventListener("click", () => {
-        answers.forEach((a) => a.classList.remove("selected"));
-        answer.classList.add("selected");
-        // Save answer text
-        answer_text = answer.querySelector(".card-text").innerText;
-        // Enable submit button
+// Chuyển sang chế độ bắt đầu trả lời câu hỏi
+function swapToQuizMode() {
+    showLoading();
+
+    getListQuiz();
+    showQuizScreen();
+}
+
+// Chuyển sang chế độ trả lời lại những câu hỏi sai
+function swapToRetryMode() {
+    currentLearningMode = listWrongQuiz;
+    checkMode();
+    showQuizScreen();
+}
+
+// Hiển thị màn hình học trắc nghiệm
+function showQuizScreen() {
+    startScreenElement.classList.add("hidden");
+    quizScreen.classList.remove("hidden");
+    retryScreen.classList.add("hidden");
+    resultScreen.classList.add("hidden");
+}
+
+// Hiển thị màn hình thông báo chuẩn bị trả lời lại những câu hỏi sai
+function showRetryScreen() {
+    showLoading();
+
+    startScreenElement.classList.add("hidden");
+    quizScreen.classList.add("hidden");
+    retryScreen.classList.remove("hidden");
+    resultScreen.classList.add("hidden");
+
+    setTimeout(function () {
+        hideLoading();
+    }, 1000);
+}
+
+// Hiển thị màn hình kết quả bài học
+function showResultScreen() {
+    showLoading();
+
+    startScreenElement.classList.add("hidden");
+    quizScreen.classList.add("hidden");
+    retryScreen.classList.add("hidden");
+    resultScreen.classList.remove("hidden");
+
+    setTimeout(function () {
+        hideLoading();
+    }, 1000);
+}
+
+// Thêm sự kiện khi chọn câu trả lời
+answerElements.forEach((aElement) => {
+    aElement.addEventListener("click", () => {
+        answerElements.forEach((a) => a.classList.remove("selected"));
+        // thêm class selected cho câu trả lời được chọn
+        aElement.classList.add("selected");
+        // lưu lại nội dung của câu trả lời được chọn
+        selectedAnswer = aElement.querySelector(".card-text").innerText;
+        // Bật nút submit
         submitButton.classList.remove("disabled");
     });
 
-    answer.addEventListener("mousedown", () => {
-        answer.classList.add("active");
+    aElement.addEventListener("mousedown", () => {
+        aElement.classList.add("active");
     });
 
-    answer.addEventListener("mouseup", () => {
-        answer.classList.remove("active");
+    aElement.addEventListener("mouseup", () => {
+        aElement.classList.remove("active");
     });
 
-    answer.addEventListener("mouseleave", () => {
+    aElement.addEventListener("mouseleave", () => {
         setTimeout(function () {
-            answer.classList.remove("active");
+            aElement.classList.remove("active");
         }, 300);
     });
 
-    answer.addEventListener("touchstart", () => {
-        answer.classList.add("active");
+    aElement.addEventListener("touchstart", () => {
+        aElement.classList.add("active");
     });
 
-    answer.addEventListener("touchend", () => {
-        answer.classList.remove("active");
+    aElement.addEventListener("touchend", () => {
+        aElement.classList.remove("active");
     });
 });
 
-buttons.forEach((button) => {
-    button.addEventListener("mousedown", () => {
-        button.classList.add("active");
+// Thêm sự kiện khi click vào các nút button
+buttonElements.forEach((bElement) => {
+    bElement.addEventListener("mousedown", () => {
+        bElement.classList.add("active");
     });
 
-    button.addEventListener("mouseup", () => {
-        button.classList.remove("active");
+    bElement.addEventListener("mouseup", () => {
+        bElement.classList.remove("active");
     });
 
-    button.addEventListener("mouseleave", () => {
+    bElement.addEventListener("mouseleave", () => {
         setTimeout(function () {
-            button.classList.remove("active");
+            bElement.classList.remove("active");
         }, 300);
     });
 
-    button.addEventListener("touchstart", () => {
-        button.classList.add("active");
+    bElement.addEventListener("touchstart", () => {
+        bElement.classList.add("active");
     });
 
-    button.addEventListener("touchend", () => {
-        button.classList.remove("active");
+    bElement.addEventListener("touchend", () => {
+        bElement.classList.remove("active");
     });
 });
 
-// Add event listener for submitting answer
+// thêm sự kiện khi nhấn vào nút submit button
 submitButton.addEventListener("click", function (event) {
-    event.preventDefault(); // Prevent page from reloading on submit
+    // Prevent page from reloading on submit
+    event.preventDefault();
 
-    if (answer_text == get_correct_answer()) {
-        display_correct();
-        play_correct_audio();
+    // set kết quả vào ô result và chuẩn bị phát âm thanh
+    if (selectedAnswer == getCorrectAnswer()) {
+        displayCorrectResult();
+        playCorrectAudio();
     } else {
-        display_wrong();
-        play_wrong_audio();
+        displayWrongResult();
+        playWrongAudio();
+
+        // Thêm vào câu sai
+        if (currentLearningMode == listQuiz) {
+            listWrongQuiz.push(listQuiz[currentQuestion]);
+        } else {
+            listWrongQuiz.push(listWrongQuiz[currentWrongQuestion]);
+        }
+        console.log(listWrongQuiz);
     }
 
-    // Show result overlay
-    resultOverlay.classList.remove("hidden");
+    // Hiển thị kết quả
+    resultOverlayElement.classList.remove("hidden");
 
-    // Enable next button after delay
+    // Bật nút sang câu tiếp theo
     setTimeout(function () {
         nextButton.classList.remove("disabled");
     }, 200);
 
-    // Disable submit button
+    // Tắt nút trả lời
     submitButton.classList.add("disabled");
 });
 
 // Add event listener for next button
 nextButton.addEventListener("click", function (event) {
     event.preventDefault();
-    reset_css_question();
+
+    resetCssElements();
+
     // Show next question
-    question_current++;
-    show_question(question_current);
+    if (currentLearningMode == listQuiz) {
+        currentQuestion++;
+    } else {
+        currentWrongQuestion++;
+    }
+    checkMode();
 });
+
+// display correct result
+function displayCorrectResult() {
+    resultModalElement.classList.remove("wrong");
+    resultModalElement.classList.add("correct");
+    resultTextElement.textContent = "Tuyệt vời!";
+    resultMessageElement.textContent = "";
+    nextButton.textContent = "Tiếp tục";
+    resultIconElement.classList.remove("bi-x-circle-fill");
+    resultIconElement.classList.add("bi-check-circle-fill");
+}
+
+// display wrong result
+function displayWrongResult() {
+    resultModalElement.classList.remove("correct");
+    resultModalElement.classList.add("wrong");
+    resultTextElement.textContent = "Không chính xác";
+    resultMessageElement.innerHTML = `<strong>Đáp án đúng: </strong>${getCorrectAnswer()}`;
+    nextButton.textContent = "Đã hiểu";
+    resultIconElement.classList.remove("bi-check-circle-fill");
+    resultIconElement.classList.add("bi-x-circle-fill");
+}
+
+// load audio elements
+function loadAudio() {
+    audioCorrect.load();
+    audioWrong.load();
+}
+
+// play correct audio
+function playCorrectAudio() {
+    audioCorrect.play();
+}
+
+// play wrong audio
+function playWrongAudio() {
+    audioWrong.play();
+}
+
+// reset CSS styles for question and answers
+function resetCssElements() {
+    // hide result overlay
+    resultOverlayElement.classList.add("hidden");
+    // disable next button
+    nextButton.classList.add("disabled");
+    // reset answer selections
+    answerElements.forEach((a) => {
+        a.classList.remove("selected");
+    });
+}
+
+function showLoading() {
+    loadElement.classList.remove("hidden");
+}
+
+function hideLoading() {
+    loadElement.classList.add("hidden");
+}
--- public/js/module.js
+++ public/js/module.js
@@ -1,31 +1,27 @@
-// get elements
-const audio_correct = document.getElementById("audio_correct");
-const audio_wrong = document.getElementById("audio_wrong");
-
-// capitalize first letter of each word in object properties
-function capitalizeFirstLetterQA(list_obj) {
-    list_obj.forEach((obj) => {
-        obj.en = obj.en.charAt(0).toUpperCase() + obj.en.slice(1);
-        obj.vi = obj.vi.charAt(0).toUpperCase() + obj.vi.slice(1);
+// Viết hoa chữ cái đầu của mỗi từ trong bộ câu hỏi
+function capitalizeFirstLetterQA(allQA) {
+    allQA.forEach((qa) => {
+        qa.en = qa.en.charAt(0).toUpperCase() + qa.en.slice(1);
+        qa.vi = qa.vi.charAt(0).toUpperCase() + qa.vi.slice(1);
     });
 
-    return list_obj;
+    return allQA;
 }
 
-// choose a random number of objects from an object array
-function choose_random_obj(obj, count) {
+// Chọn ngẫu nhiên một số đối tượng từ một mảng đối tượng
+function chooseRandomObj(obj, count) {
     const keys = Object.keys(obj);
     const randomKeys = keys.sort(() => Math.random() - 0.5).slice(0, count);
     const randomValues = randomKeys.map((key) => obj[key]);
     return randomValues;
 }
 
-// Select random elements from an array
-function choose_random_elements(arr, count, excluded_element) {
+// Chọn các phần tử ngẫu nhiên từ một mảng
+function chooseRandomArr(arr, count, excludedElement) {
     const chosenIndexes = new Set();
     while (chosenIndexes.size < count) {
         let index = Math.floor(Math.random() * arr.length);
-        if (arr[index] === excluded_element) {
+        if (arr[index] === excludedElement) {
             continue;
         }
         chosenIndexes.add(index);
@@ -33,8 +29,8 @@ function choose_random_elements(arr, count, excluded_element) {
     return Array.from(chosenIndexes).map((index) => arr[index]);
 }
 
-// shuffle an array randomly
-function shuffleArray(array) {
+// Xáo trộn một mảng ngẫu nhiên
+function shuffleArr(array) {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
@@ -42,60 +38,10 @@ function shuffleArray(array) {
     return array;
 }
 
-// shuffle an object randomly
-function shuffleObject(obj) {
+// Xáo trộn một đối tượng ngẫu nhiên
+function shuffleObj(obj) {
     const shuffledKeys = Object.keys(obj).sort(() => Math.random() - 0.5);
     const shuffledObject = {};
     shuffledKeys.forEach((key) => (shuffledObject[key] = obj[key]));
     return shuffledObject;
-}
-
-// display correct result
-function display_correct() {
-    resultModal.classList.remove("wrong");
-    resultModal.classList.add("correct");
-    resultText.textContent = "Tuyệt vời!";
-    resultMessage.textContent = "";
-    nextButton.textContent = "Tiếp tục";
-    resultIcon.classList.remove("bi-x-circle-fill");
-    resultIcon.classList.add("bi-check-circle-fill");
-}
-
-// display wrong result
-function display_wrong() {
-    resultModal.classList.remove("correct");
-    resultModal.classList.add("wrong");
-    resultText.textContent = "Không chính xác";
-    resultMessage.innerHTML = `<strong>Đáp án đúng: </strong>${get_correct_answer()}`;
-    nextButton.textContent = "Đã hiểu";
-    resultIcon.classList.remove("bi-check-circle-fill");
-    resultIcon.classList.add("bi-x-circle-fill");
-}
-
-// load audio elements
-function load_audio() {
-    audio_correct.load();
-    audio_wrong.load();
-}
-
-// play correct audio
-function play_correct_audio() {
-    audio_correct.play();
-}
-
-// play wrong audio
-function play_wrong_audio() {
-    audio_wrong.play();
-}
-
-// reset CSS styles for question and answers
-function reset_css_question() {
-    // hide result overlay
-    resultOverlay.classList.add("hidden");
-    // disable next button
-    nextButton.classList.add("disabled");
-    // reset answer selections
-    answers.forEach((answer) => {
-        answer.classList.remove("selected");
-    });
 }
